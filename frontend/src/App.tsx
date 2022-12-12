313220b0198a850e52954dcf76960b4f4889a797
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-12 10:42:06 +0900

    web3 install

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import {Button} from '@chakra-ui/react';
 import Main from "./routes/Main";
 
 const App: FC = () => {
+  const [account, setAccount] = useState();
+
+  const getAccount = async () => {
+    try {
+      if(window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+
+        setAccount(accounts[0]);
+        console.log(account);
+      } else {
+        alert("install metamask");
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getAccount();
+  }, [account]);
+
   return (
     <Router>
       <Routes>
--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="react-scripts" />
 
+// 윈도우라는 객체에서 이더리움이라는 타입을 인식하게 지정해줌
 interface Window {
   ethereum: any;
 }
