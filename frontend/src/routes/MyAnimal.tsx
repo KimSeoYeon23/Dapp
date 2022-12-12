9b872738763724a3dc69be230a0b1ced78dd79f3
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-12 16:52:11 +0900

    MyAnimal editting

--- frontend/src/routes/MyAnimal.tsx
+++ frontend/src/routes/MyAnimal.tsx
@@ -1,10 +1,20 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 interface MyAnimalProps {
     account: string;
 }
 
 const MyAnimal: FC<MyAnimalProps> = ({account}) => {
+    const [animalCardArray, setAnimalCardArray] = useState<string[]>();
+
+    const getAnimalTokens = async () => {
+        try {
+                
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div>
             My Animal
