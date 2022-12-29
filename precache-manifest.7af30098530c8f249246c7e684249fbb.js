ec81a7f86d76561b16e354b8257e1eab3d75656e
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-29 09:51:02 +0900

    Updates

---
+++ precache-manifest.7af30098530c8f249246c7e684249fbb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "40edef4c8f1e82a9e605",
+    "url": "/Dapp/static/js/main.a7f41e84.chunk.js"
+  },
+  {
+    "revision": "b32cdd6840288d29d236",
+    "url": "/Dapp/static/js/runtime~main.3f041539.js"
+  },
+  {
+    "revision": "029c508674924230ee5b",
+    "url": "/Dapp/static/js/2.75e6eba9.chunk.js"
+  },
+  {
+    "revision": "98c19dd4a8dfc7236a8f577a9040f2d7",
+    "url": "/Dapp/index.html"
+  }
+];
